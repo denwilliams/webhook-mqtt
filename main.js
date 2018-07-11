7fb63b08ed7304479fcab7f5549953a05063df3b
Den Williams <work@denwilliams.net>
2018-07-11 22:46:17 +1000

    first version

---
+++ main.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const express = require('express');
+const mqttusvc = require('mqtt-usvc');
+const bodyParser = require('body-parser');
+
+const service = mqttusvc.create();
+
+const app = express();
+
+app.use((req, res, next) => {
+  const authorization = req.headers.authorization || '';
+  const token = authorization.replace('Bearer ', '');
+
+  if (!service.config.tokens.includes(token)) {
+    res.status(403).send('Invalid token');
+    return;
+  }
+
+  next();
+});
+
+app.use(bodyParser.json());
+
+service.config.hooks.forEach(h => {
+  app[h.method](h.path, (req, res) => {
+    console.log(`${h.path} => ${h.topic}`);
+    service.send(h.topic, h.message || req.body, { retain: false });
+    res.status(200).send('OK');
+  });
+});
+
+app.listen(service.config.port);
+
