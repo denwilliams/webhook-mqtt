b4814803141bca0851cd279e81e1fd07ee8e45cd
Den Williams <work@denwilliams.net>
2018-07-11 23:36:07 +1000

    support bool

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 service.config.hooks.forEach(h => {
   app[h.method](h.path, (req, res) => {
     console.log(`${h.path} => ${h.topic}`);
-    service.send(h.topic, h.message || req.body, { retain: false });
+    service.send(h.topic, h.message === undefined ? req.body : h.message, { retain: false });
     res.status(200).send('OK');
   });
 });
